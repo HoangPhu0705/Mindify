a5e8fa45c8527ecec15f8faddc2a437c3ed761c4
HoangPhu0705 <phuhoang07051003@gmail.com>
2024-05-24 21:19:58 +0700

    fix multidex error, gradle bug

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:frontend/firebase_options.dart';
 import 'package:frontend/auth/sign_in.dart';
+import 'package:frontend/firebase_options.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MindifyApp());
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
 }
 
 class MindifyApp extends StatefulWidget {
   const MindifyApp({super.key});
 
   @override
-  State<MindifyApp> createState() => MindifyAppState();
+  State<MindifyApp> createState() => _MindifyAppState();
 }
 
-class MindifyAppState extends State<MindifyApp> {
+class _MindifyAppState extends State<MindifyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
---
+++ frontend/lib/utils/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Colors
+  static const Color charcoal = Color(0xFF333333);
+  static const Color cream = Color(0xFFFFFDD0);
+  static const Color red = Color(0xFFFF0000);
+
+  // Secondary Colors
+  static const Color pink = Color(0xFFFFC0CB);
+  static const Color pastelPink = Color(0xFFFFD1DC);
+  static const Color blue = Color(0xFF0000FF);
+  static const Color pastelBlue = Color(0xFFB0E0E6);
+  static const Color green = Color(0xFF008000);
+  static const Color pastelGreen = Color(0xFF98FB98);
+  static const Color gold = Color(0xFFFFD700);
+  static const Color pastelYellow = Color(0xFFFFFFE0);
+}
