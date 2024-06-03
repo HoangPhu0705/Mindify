8318b14c9522df445284b3e195c91a07b0663f04
HoangPhu0705 <phuhoang07051003@gmail.com>
2024-06-03 17:47:23 +0700

    update setting page

--- frontend/lib/pages/setting_page.dart
+++ frontend/lib/pages/setting_page.dart
@@ -25,23 +25,58 @@ class _SettingPageState extends State<SettingPage> {
             style: TextStyle(fontSize: 24),
           ),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _buildSectionTitle("Profile"),
-            _buildListTile(
-              "Edit Profile",
-              Icons.chevron_right_outlined,
-              () {},
-            ),
-            Divider(),
-            _buildSectionTitle("Notification"),
-            _buildListTile2(
-              "Learning reminders",
-              () {},
-            ),
-            Divider(),
-          ],
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildSectionTitle("Profile"),
+              _buildListTile(
+                "Edit Profile",
+                Icons.chevron_right_outlined,
+                () {},
+              ),
+              Divider(),
+              _buildSectionTitle("Notification"),
+              _buildListTileNotify(
+                "Learning reminders",
+                "Set aside time every week for learning",
+                () {},
+              ),
+              Divider(),
+              _buildSectionTitle("About"),
+              _buildListTile(
+                "Terms of Service",
+                Icons.chevron_right_outlined,
+                () {},
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0),
+                child: Divider(),
+              ),
+              _buildListTile(
+                "Privacy Policy",
+                Icons.chevron_right_outlined,
+                () {},
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0),
+                child: Divider(),
+              ),
+              _buildListTile(
+                "Teacher Rules and Requirements",
+                Icons.chevron_right_outlined,
+                () {},
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0),
+                child: Divider(),
+              ),
+              _buildListTileDeleteAccount(
+                "Delete Account",
+                () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -74,14 +109,16 @@ class _SettingPageState extends State<SettingPage> {
     );
   }
 
-  Widget _buildListTile2(String title, VoidCallback onTap) {
+  Widget _buildListTileNotify(
+      String title, String subtitle, VoidCallback onTap) {
     return ListTile(
+      subtitle: Text(
+        subtitle,
+        style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontSize: 12),
+      ),
       title: Text(
         title,
-        style: Theme.of(context)
-            .textTheme
-            .bodyLarge!
-            .copyWith(fontWeight: FontWeight.w400),
+        style: Theme.of(context).textTheme.bodyLarge!,
       ),
       trailing: GestureDetector(
         onTap: onTap,
@@ -94,6 +131,33 @@ class _SettingPageState extends State<SettingPage> {
               )),
           child: Text(
             "Manage",
+            style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildListTileDeleteAccount(String title, VoidCallback onTap) {
+    return ListTile(
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.bodyLarge!,
+      ),
+      trailing: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: Colors.black,
+              )),
+          child: Text(
+            "Delete my account",
             style: Theme.of(context).textTheme.titleSmall!.copyWith(
                   fontSize: 18,
                   fontWeight: FontWeight.w600,
