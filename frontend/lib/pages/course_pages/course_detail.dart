d15b1b0cb0704a7017616ff5a35a5c23dc1981ca
HoangPhu0705 <phuhoang07051003@gmail.com>
2024-06-13 15:48:17 +0700

    lesson tab

--- frontend/lib/pages/course_pages/course_detail.dart
+++ frontend/lib/pages/course_pages/course_detail.dart
@@ -1,10 +1,16 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:floating_tabbar/Widgets/top_tabbar.dart';
 import 'package:floating_tabbar/lib.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:chewie/chewie.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_animated_button/flutter_animated_button.dart';
+import 'package:frontend/pages/course_pages/lesson_tab.dart';
 import 'package:frontend/utils/colors.dart';
+import 'package:frontend/utils/spacing.dart';
+import 'package:frontend/utils/styles.dart';
 import 'package:frontend/widgets/video_player_view.dart';
 import 'package:video_player/video_player.dart';
 import 'package:floating_tabbar/floating_tabbar.dart';
@@ -16,37 +22,28 @@ class CourseDetail extends StatefulWidget {
   State<CourseDetail> createState() => _CourseDetailState();
 }
 
-class _CourseDetailState extends State<CourseDetail> {
+class _CourseDetailState extends State<CourseDetail>
+    with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+  bool isFollowed = false;
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(length: 3, vsync: this);
   }
 
-  List<String> todos = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-    "23"
-  ];
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _tabController?.dispose();
+    super.dispose();
+  }
+
+  Future<void> followUser() async {
+    setState(() {
+      isFollowed = !isFollowed;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -80,16 +77,36 @@ class _CourseDetailState extends State<CourseDetail> {
                   "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4",
               dataSourceType: DataSourceType.network,
             ),
-            // Expanded(
-            //   child: ListView.builder(
-            //     itemCount: todos.length,
-            //     itemBuilder: (context, index) {
-            //       return ListTile(
-            //         title: Text(todos[index]),
-            //       );
-            //     },
-            //   ),
-            // ),
+            TabBar(
+              controller: _tabController,
+              splashFactory: NoSplash.splashFactory,
+              tabs: const [
+                Tab(text: 'Lessons'),
+                Tab(text: 'Projects'),
+                Tab(text: 'Discussions'),
+              ],
+              labelColor: Colors.black,
+              labelStyle: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+              ),
+              indicatorSize: TabBarIndicatorSize.tab,
+              indicatorColor: Colors.black,
+              indicatorWeight: 3,
+            ),
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  LessonTab(
+                    isFollowed: isFollowed,
+                    followUser: followUser,
+                  ),
+                  Center(child: Text("Projects")),
+                  Center(child: Text("Discussions")),
+                ],
+              ),
+            ),
           ],
         ),
       ),
