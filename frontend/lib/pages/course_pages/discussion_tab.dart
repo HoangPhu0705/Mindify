e466159e7dd26189875ee8637df86b31cf0b4ac2
HoangPhu0705 <phuhoang07051003@gmail.com>
2024-06-18 17:38:16 +0700

    modify discussion tab

--- frontend/lib/pages/course_pages/discussion_tab.dart
+++ frontend/lib/pages/course_pages/discussion_tab.dart
@@ -3,6 +3,7 @@
 import 'dart:developer';
 
 import 'package:comment_tree/comment_tree.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/utils/colors.dart';
 import 'package:frontend/utils/spacing.dart';
@@ -17,16 +18,47 @@ class Discussion extends StatefulWidget {
 class _DiscussionState extends State<Discussion> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        child: SingleChildScrollView(
+    return Scaffold(
+      bottomSheet: Container(
+        height: MediaQuery.of(context).size.height * 0.07,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              offset: Offset(0, -1),
+            ),
+          ],
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: AppColors.ghostWhite,
+            contentPadding: EdgeInsets.all(12),
+            hintText: 'Start a discussion...',
+            border: InputBorder.none,
+            hintStyle:
+                Theme.of(context).textTheme.bodySmall!.copyWith(fontSize: 16),
+            suffixIcon: IconButton(
+              onPressed: () {
+                log('Comment submitted');
+              },
+              icon: Icon(Icons.send, color: AppColors.deepBlue),
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(
+              8, 12, 8, MediaQuery.of(context).size.height * 0.07),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Align(
                 alignment: Alignment.topLeft,
                 child: Text(
-                  '2 Discussions',
+                  '4 Discussions',
                   style: Theme.of(context).textTheme.titleMedium!.copyWith(
                         color: Colors.black,
                         fontWeight: FontWeight.bold,
@@ -39,27 +71,33 @@ class _DiscussionState extends State<Discussion> {
                 physics: const NeverScrollableScrollPhysics(),
                 itemCount: 4,
                 itemBuilder: (context, index) {
-                  return _buildCommentTree(
-                    context,
-                    Comment(
-                      avatar: 'null',
-                      userName: 'Hiếu Phạm',
-                      content: 'This course is great !!',
-                    ),
-                    [
-                      Comment(
-                        avatar: 'null',
-                        userName: 'Phú Phan',
-                        content: 'Thank you',
+                  return Column(
+                    children: [
+                      _buildCommentTree(
+                        context,
+                        Comment(
+                          avatar: 'null',
+                          userName: 'Hiếu Phạm',
+                          content: 'This course is great !!',
+                        ),
+                        [
+                          Comment(
+                            avatar: 'null',
+                            userName: 'Phú Phan',
+                            content: 'Thank you',
+                          ),
+                        ],
+                      ),
+                      Divider(
+                        color: AppColors.lighterGrey,
                       ),
                     ],
                   );
                 },
-              )
+              ),
             ],
           ),
         ),
-        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
       ),
     );
   }
