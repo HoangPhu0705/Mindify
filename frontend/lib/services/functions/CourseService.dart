43df1d5d9ddb0accb9457a02b12407b793f37751
HoangPhu0705 <phuhoang07051003@gmail.com>
2024-07-03 10:40:26 +0700

    update

--- frontend/lib/services/functions/CourseService.dart
+++ frontend/lib/services/functions/CourseService.dart
@@ -59,7 +59,6 @@ class CourseService {
       final response = await http.get(Uri.parse("$baseUrl/courses/newest"));
       if (response.statusCode == 200) {
         List<dynamic> data = json.decode(response.body);
-        log("5 newest Courses: $data");
         return data.map((course) => Course.fromJson(course)).toList();
       } else {
         throw Exception("Failed to load newest 5 courses");
