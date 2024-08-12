bfcf36bb38a4b772fb6759a8940cee190337f14e
HieuPham-origin <hieupham112003@gmail.com>
2024-08-12 23:18:58 +0700

    testinggg

--- backend/src/tests/authenticate.test.js
+++ backend/src/tests/authenticate.test.js
@@ -1,17 +1,44 @@
-const { loginUser } = require('../app/service/AdminService');
+const { initializeApp } = require('firebase/app');
+// const { getAuth } = require('firebase-admin/auth');
+const { getAuth, signInWithEmailAndPassword } = require('firebase/auth');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const auth = getAuth(firebaseApp);
+const signInUser = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const token = await userCredential.user.getIdToken();
+    return token;
+  } catch (error) {
+    console.error('Error signing in:', error);
+  }
+};
 let token;
 describe('Firebase Authentication', () => {
   it('should log in and retrieve ID token', async () => {
     const email = 'hieuga678902003@gmail.com';
     const password = '123456';
 
-    const idToken = await loginUser(email, password);
-    token = idToken.token
-    expect(idToken).toBeDefined();
-    console.log('Retrieved ID token:', JSON.stringify(idToken.token));
+    token = await signInUser(email, password);
+    // token = idToken.token
+    expect(token).toBeDefined();
+    console.log('Retrieved ID token:', JSON.stringify(token));
     console.log('Token:', token); // Thêm dòng này trong test của bạn
 
   });
 });
 
-module.exports = { token };
+module.exports = { signInUser };
--- backend/src/tests/course.test.js
+++ backend/src/tests/course.test.js
@@ -1,19 +1,24 @@
 const request = require('supertest');
 const app = require('../app');
-const { token } = require('./authenticate.test');
+const { signInUser } = require('./authenticate.test');
+let token
+beforeAll(async () => {
+    const email = 'hieuga678902003@gmail.com';
+    const password = '123456';
+    const idToken = await signInUser(email, password);
+    token = idToken;
+  });
 
-describe('Course Routes', () => {
-  it('should search courses', async () => {
-    const query = 'code';
-    
+describe('POST /courses/top5', () => {
+  it('should get top5 courses', async () => {
+    console.log(token)
     const res = await request(app)
-      .post('/api/courses/searchCourses')
-      .set('Authorization', `Bearer ${token}`)
-      .send({ query });
+      .get('/api/courses/top5')
+      .set('Authorization', `Bearer ${token}`);
 
     console.log('Response Status:', res.statusCode);  // Kiểm tra mã trạng thái phản hồi
     console.log('Response Body:', res.body);  // Kiểm tra nội dung phản hồi
-    
+
     expect(res.statusCode).toEqual(200);
     expect(res.body.courses.length).toBeGreaterThan(0);
   });
